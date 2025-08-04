470b2b94b12c7f1408ed581c62d66e5745456ab6
Samuel <henrisamuel234@gmail.com>
2025-08-04 18:51:54 -0300

    Variaveis

---
+++ src/Modulo1/Declarando_variaveis.java
@@ -0,0 +1,16 @@
+package Modulo1;
+
+public class Declarando_variaveis {
+
+    public static <Int> void main(String[] args) {
+
+        String nome = "Samuel";
+        System.out.println(nome);
+
+        int idade = 19;
+        System.out.println(idade);
+
+        double altura = 1.75;
+        System.out.println(altura);
+    }
+}
