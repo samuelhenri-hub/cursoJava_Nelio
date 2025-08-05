12ef6ee67375a5e2f066f4adf218180816199e34
Samuel <henrisamuel234@gmail.com>
2025-08-05 20:12:16 -0300

    Exercicios sobre entrada de dados versão final

--- src/Modulo1/entradaDados.java
+++ src/Modulo1/entradaDados.java
@@ -1,37 +1,31 @@
 package Modulo1;
 
-import javax.xml.transform.stream.StreamSource;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class entradaDados {
 
-    private static Locale locale;
 
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        locale.setDefault(locale.US);
 
         System.out.print("Digite seu nome: ");
         String nome;
         nome = sc.next();
-        System.out.println("Seu nome é: " + nome);
 
         System.out.print("Digite sua idade: ");
         int idade;
         idade = sc.nextInt();
-        System.out.println("Sua idade é: " + idade);
 
         System.out.print("Digite sua altura: ");
         double altura;
         altura = sc.nextDouble();
-        System.out.println("Sua altura é: " + altura);
 
         System.out.print("Digite seu sexo: ");
         char genero;
         genero = sc.next().charAt(0);
-        System.out.println("O Seu genero é: " + genero);
+
+        System.out.printf("Seu nome é: %s %n Você tem %d anos %n Sua altura é de %.2f metros%n Seu sexo é: %s ", nome, idade, altura, genero);
 
         sc.close();
     }
