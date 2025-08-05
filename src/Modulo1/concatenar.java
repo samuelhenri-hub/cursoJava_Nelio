31c57f77655ea9377522c2da896c3cbe8edc272a
Samuel <henrisamuel234@gmail.com>
2025-08-05 17:44:45 -0300

    Concatenar aula 3

---
+++ src/Modulo1/concatenar.java
@@ -0,0 +1,14 @@
+package Modulo1;
+
+public class concatenar {
+
+    public static void main(String[] args) {
+
+        double alt = 1.753456;
+        System.out.println(alt);
+        System.out.printf("%.2f%n", alt);
+
+        System.out.println("Minha altura=" + alt + "Metros");
+        System.out.printf("Minha altura= %.2f metros %n", alt);
+    }
+}
