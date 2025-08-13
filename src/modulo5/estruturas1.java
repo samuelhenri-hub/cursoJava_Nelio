04e6dd7842884b9b2b7e28936d34006e0817af45
Samuel <henrisamuel234@gmail.com>
2025-08-13 17:16:57 -0300

    modulo 5, estruturas

---
+++ src/modulo5/estruturas1.java
@@ -0,0 +1,16 @@
+package modulo5;
+
+import java.util.Scanner;
+
+public class estruturas1 {
+    public static void main(String [] args) {
+
+        Scanner sc = new Scanner (System.in);
+
+        int hora;
+        System.out.print("Que horas são? ");
+        hora = sc.nextInt();
+
+        if ()
+    }
+}
