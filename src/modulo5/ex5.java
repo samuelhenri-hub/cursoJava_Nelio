344c44450c20252636c25cda9b6ce05054413fb2
Samuel <henrisamuel234@gmail.com>
2025-08-20 17:23:01 -0300

    EXERCICIO 5 (corrigido)

--- src/modulo5/ex5.java
+++ src/modulo5/ex5.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class ex5 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int codigo = sc.nextInt();
+        int qntde = sc.nextInt();
+
+        double total;
+        if (codigo == 1) {
+            total = qntde * 4;
+        }
+        else if (codigo == 2) {
+            total = qntde * 4.5;
+        }
+        else if (codigo == 3) {
+            total = qntde * 5;
+        }
+        else if (codigo == 4) {
+            total = qntde * 2;
+        }
+        else {
+            total = qntde * 1.50;
+        }
+
+        System.out.printf("TOTAL: %.2f", total);
         sc.close();
     }
 }
