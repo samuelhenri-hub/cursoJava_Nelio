ea94498cfaaa9971ed042fec385d697aef7ca9c2
Samuel <henrisamuel234@gmail.com>
2025-08-20 17:52:44 -0300

    EXERCICIO 6 (corrigido)

--- src/modulo5/ex6.java
+++ src/modulo5/ex6.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class ex6 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        double num = sc.nextDouble();
+
+        if (num >= 0 && num <=25) {
+            System.out.print("Intervalo: [0,25]");
+        }
+        else if (num >= 25 && num <=50) {
+                System.out.print("Intervalo: [25,50]");
+        }
+        else if (num >= 50 && num <=75) {
+            System.out.print("Intervalo: [50, 75]");
+        }
+        else if (num >= 75 && num <=100) {
+            System.out.print("Intervalo: [75, 100]");
+        }
+        else {
+            System.out.print("FORA DO INTERVALO!");
+        }
         sc.close();
     }
 }
