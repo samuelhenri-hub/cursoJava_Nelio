ec56dd7647c793d68881c86331a5057d42cbbcb8
Samuel <henrisamuel234@gmail.com>
2025-08-20 17:24:34 -0300

    EXERCICIO 6

---
+++ src/modulo5/ex6.java
@@ -0,0 +1,10 @@
+package modulo5;
+
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
