3dd252e58a0a9aae6ff80f66510b78f0f8cb45ab
Samuel <henrisamuel234@gmail.com>
2025-08-25 17:34:28 -0300

    EXERCICIO 2 FINALIZADO

--- src/modulo6_while/Ex2.java
+++ src/modulo6_while/Ex2.java
@@ -1,4 +1,34 @@
 package modulo6_while;
 
+import java.util.Scanner;
+
 public class Ex2 {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 || y != 0) {
+
+
+            if (x > 0 && y > 0) {
+                System.out.println("QUADRANTE 1");
+            }
+            else if (x < 0 && y > 0) {
+                System.out.println("QUADRANTE 2");
+            }
+            else if (x < 0 && x < 0) {
+                System.out.println("QUADRANTE 3");
+            }
+            else if (x > 0 && y < 0) {
+                System.out.println("QUADRANTE 4");
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+        sc.close();
+    }
 }
