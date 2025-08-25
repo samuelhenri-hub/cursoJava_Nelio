0f43b14b611715f05106bafaf30bbab429835b65
Samuel <henrisamuel234@gmail.com>
2025-08-25 17:08:24 -0300

    EXERCICIO 1 certo

--- src/modulo6_while/ex1.java
+++ src/modulo6_while/ex1.java
@@ -1,4 +1,21 @@
 package modulo6_while;
 
+import java.util.Scanner;
+
 public class ex1 {
+    public static void main (String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("DIGITE SUA SENHA");
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("SENHA INVALIDA");
+            senha = sc.nextInt();
+        }
+        System.out.println("SENHA CORRETA");
+
+        sc.close();
+    }
 }
