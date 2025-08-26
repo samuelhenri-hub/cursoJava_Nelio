a84a76fdca45af7ff5480a6cb940f2dac12fd907
Samuel <henrisamuel234@gmail.com>
2025-08-26 17:08:04 -0300

    EXERCICIO 5 corrigido

--- src/modulo6_while/Ex5.java
+++ src/modulo6_while/Ex5.java
@@ -1,4 +1,25 @@
 package modulo6_while;
 
+import java.util.Scanner;
+
 public class Ex5 {
+    public static void main (String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+
+        for (int x = 0; x <= n; x++) {
+            if (x >= 10 && x <= 20) {
+                in += + 1;
+            }
+            else {
+                out += + 1;
+            }
+            System.out.println("IN: " + in + "OUT: " + out);
+        }
+        sc.close();
+    }
 }
