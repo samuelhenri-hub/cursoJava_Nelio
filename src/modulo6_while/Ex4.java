4e305530688b998d46ca46017d7a95ae4bf04e8c
Samuel <henrisamuel234@gmail.com>
2025-08-26 16:52:23 -0300

    EXERCICIO 4

---
+++ src/modulo6_while/Ex4.java
@@ -0,0 +1,18 @@
+package modulo6_while;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x ; i++ ) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
