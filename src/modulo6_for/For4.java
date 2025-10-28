c87ffdb2751036d8f708916a591481b56b0b4ecf
Samuel <henrisamuel234@gmail.com>
2025-10-28 16:15:21 -0300

    Exercicio for 4

---
+++ src/modulo6_for/For4.java
@@ -0,0 +1,26 @@
+package modulo6_for;
+
+import java.util.Scanner;
+
+public class For4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            if (b == 0) {
+                System.out.print("Divisao impossivel");
+            }
+            else {
+                double divisao = a / b;
+                System.out.print(divisao);
+            }
+        }
+
+        sc.close();
+    }
+}
