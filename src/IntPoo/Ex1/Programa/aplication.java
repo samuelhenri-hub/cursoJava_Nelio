53cd5b83c540d953eedd047102fff971b538fbf6
Samuel <henrisamuel234@gmail.com>
2025-10-30 17:53:59 -0300

    recebendo atributos objeto

--- src/IntPoo/Ex1/Programa/aplication.java
+++ src/IntPoo/Ex1/Programa/aplication.java
@@ -2,6 +2,7 @@ package IntPoo.Ex1.Programa;
 
 import IntPoo.Ex1.Entidades.retangulo1;
 
+import java.awt.*;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,9 +15,15 @@ public class aplication {
         x = new retangulo1();
         y = new retangulo1();
 
+        System.out.println("Medidas do triangulo x: ");
         x.ladoA = sc.nextDouble();
         x.ladoB = sc.nextDouble();
         x.ladoC = sc.nextDouble();
+
+        System.out.println("Medidas do triangulo y: ");
+        y.ladoA = sc.nextDouble();
+        y.ladoB = sc.nextDouble();
+        y.ladoC = sc.nextDouble();
 
         sc.close();
     }
