0a78b5a6fc52898980e2ec020149de1a39de2c44
Samuel <henrisamuel234@gmail.com>
2025-10-30 17:50:42 -0300

    recebendo atributos objeto

--- src/IntPoo/Ex1/Programa/aplication.java
+++ src/IntPoo/Ex1/Programa/aplication.java
@@ -1,4 +1,23 @@
 package IntPoo.Ex1.Programa;
 
+import IntPoo.Ex1.Entidades.retangulo1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class aplication {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        retangulo1 x, y;
+        x = new retangulo1();
+        y = new retangulo1();
+
+        x.ladoA = sc.nextDouble();
+        x.ladoB = sc.nextDouble();
+        x.ladoC = sc.nextDouble();
+
+        sc.close();
+    }
 }
