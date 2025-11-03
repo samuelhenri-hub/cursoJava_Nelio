c455a43028f2e32695355225604d4ca9c21faca6
Samuel <henrisamuel234@gmail.com>
2025-11-03 18:27:28 -0300

    Atributos ex2

--- src/IntPoo/Ex1/Programa/Programa.java
+++ src/IntPoo/Ex1/Programa/Programa.java
@@ -1,7 +1,19 @@
 package IntPoo.Ex1.Programa;
 
+import IntPoo.Ex1.Entidades.produtos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        produtos produtos = new produtos();
+
+        System.out.println("Digite as seguintes informações do produto");
+
+        sc.close();
     }
 }
