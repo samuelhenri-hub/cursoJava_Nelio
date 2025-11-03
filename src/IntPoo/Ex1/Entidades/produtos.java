c300810719ffb78f08bc6e0a60df4ecc83533151
Samuel <henrisamuel234@gmail.com>
2025-11-03 18:22:10 -0300

    Atributos ex2

--- src/IntPoo/Ex1/Entidades/produtos.java
+++ src/IntPoo/Ex1/Entidades/produtos.java
@@ -4,4 +4,16 @@ public class produtos {
     public String nome;
     public double preco;
     public int quantidade;
+
+    public double totalEstoque() {
+        return preco * quantidade;
+    }
+
+    public void addEstoque (int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerProdutos (int quantidade) {
+        this.quantidade -= quantidade;
+    }
 }
